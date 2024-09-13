1c4296cf9ef5ad54536451defc5988e5ac0c161c
Valērijs Kostjukevičs <166384039+23DP4VKost@users.noreply.github.com>
2024-09-13 11:26:39 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,9 +1,33 @@
 package lv.rvt;
 
+import java.util.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
+        System.out.println("Hello world");
+        System.out.println("... and the universe!");
+
+        //sout = print
         // Make your Hello World programm!
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("...");
+        String name1  = scanner.nextLine();
+        
+        System.out.println("...");
+        String surname  = scanner.nextLine();
+      
+        System.out.println("...");
+        String grupa  = scanner.nextLine();
+        
+        System.out.println("Studens: " + name1 +" " + surname + ", "+" "+"grupa:"+" "+grupa);
+   
+
+
+
     }
 }
