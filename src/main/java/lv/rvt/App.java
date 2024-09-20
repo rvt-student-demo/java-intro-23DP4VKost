f80c16ff8f5c957790d444d06b780e4d4da0bfd7
Valērijs Kostjukevičs <166384039+23DP4VKost@users.noreply.github.com>
2024-09-20 10:35:52 +0000

    Simple calculator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,26 +6,35 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello world");
-        System.out.println("... and the universe!");
-
-        //sout = print
-        // Make your Hello World programm!
-
+        //Scanner scanner = new Scanner(System.in);
+        //String text = scanner.nextLine();  string no user
+        //int integer = Integer.valueOf(scanner.nextLine());  int  no user
+        //double floatingPoint = Double.valueOf(scanner.nextLine()); decimal no user
+        //boolean trueOrFalse = Boolean.valueOf(scanner.nextLine()); t/f    
 
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Give the first number:");
 
-        System.out.println("...");
-        String name1  = scanner.nextLine();
+        int integerFirst = Integer.valueOf(scanner.nextLine()); 
         
-        System.out.println("...");
-        String surname  = scanner.nextLine();
-      
-        System.out.println("...");
-        String grupa  = scanner.nextLine();
-        
-        System.out.println("Studens: " + name1 +" " + surname + ", "+" "+"grupa:"+" "+grupa);
-   
+
+        System.out.println("Give the second number:");
+
+        int integerSecond = Integer.valueOf(scanner.nextLine());
+
+        String valueFirst = String.valueOf(integerFirst);
+        String valueSecond = String.valueOf(integerSecond);
+
+        int sum = integerFirst + integerSecond;
+        int min = integerFirst - integerSecond;
+        int mult = integerFirst * integerSecond;
+        double div = integerFirst / integerSecond;
+
+        System.out.println(valueFirst+" + "+valueSecond+" = "+" "+ sum);
+        System.out.println(valueFirst+" - "+valueSecond+" = "+" "+ min);
+        System.out.println(valueFirst+" * "+valueSecond+" = "+" "+ mult);
+        System.out.println(valueFirst+" / "+valueSecond+" =" +" "+ div);
 
 
 
