1e02d1fe129a221ff116a53b83b3d429eadf8226
Valērijs Kostjukevičs <166384039+23DP4VKost@users.noreply.github.com>
2024-09-27 09:39:00 +0000

    Speeding Ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -12,31 +12,11 @@ public class App
         //double floatingPoint = Double.valueOf(scanner.nextLine()); decimal no user
         //boolean trueOrFalse = Boolean.valueOf(scanner.nextLine()); t/f    
 
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Give the first number:");
-
-        int integerFirst = Integer.valueOf(scanner.nextLine()); 
-        
-
-        System.out.println("Give the second number:");
-
-        int integerSecond = Integer.valueOf(scanner.nextLine());
-
-        String valueFirst = String.valueOf(integerFirst);
-        String valueSecond = String.valueOf(integerSecond);
-
-        int sum = integerFirst + integerSecond;
-        int min = integerFirst - integerSecond;
-        int mult = integerFirst * integerSecond;
-        double div = integerFirst / integerSecond;
-
-        System.out.println(valueFirst+" + "+valueSecond+" = "+" "+ sum);
-        System.out.println(valueFirst+" - "+valueSecond+" = "+" "+ min);
-        System.out.println(valueFirst+" * "+valueSecond+" = "+" "+ mult);
-        System.out.println(valueFirst+" / "+valueSecond+" =" +" "+ div);
-
-
-
+    System.out.println("Give speed:");
+    Scanner scanner = new Scanner((System.in));
+    int speed = Integer.valueOf(scanner.nextLine());
+    if (speed > 120) {
+        System.out.println("Speeding ticket!");
     }
+}
 }
