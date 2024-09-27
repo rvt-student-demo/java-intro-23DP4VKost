ebff10c0e830555af510a5508299322f486d7944
Valērijs Kostjukevičs <166384039+23DP4VKost@users.noreply.github.com>
2024-09-27 11:41:38 +0000

    Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -12,45 +12,40 @@ public class App
         //double floatingPoint = Double.valueOf(scanner.nextLine()); decimal no user
         //boolean trueOrFalse = Boolean.valueOf(scanner.nextLine()); t/f    
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Value of the gift?");
+        double value = Double.valueOf(scanner.nextLine());
+   
+        
+        if (value < 5000) {
+            System.out.println("No tax!");
+        } 
 
-       System.out.println("Give points[0-100]:");
-
-       Scanner scanner = new Scanner(System.in);
-       int grade = Integer.valueOf(scanner.nextLine());
-
-       
-       if (grade < 0 ) {
-        System.out.println("Impossible");
-       } 
-       
-       else if (grade >= 0 && grade <= 49){
-        System.out.println("failed");
-       }
-
-       else if (grade >= 50 && grade <= 59){
-        System.out.println("1");
-       }
-
-       else if (grade >= 60 && grade <= 69){
-        System.out.println("2");
-       }
-
-       else if (grade >= 70 && grade <= 79){
-        System.out.println("3");
-       }
-
-       else if (grade >= 80 && grade <= 89){
-        System.out.println("4");
-       }
-
-       else if (grade <= 100){
-        System.out.println('5');
-       }
-
-       else {
-        System.out.println("incredible!");
-       }
+        else if (value >= 5000 && value < 25000){
+            double fitst_tax = (100 + (value - 5000)*0.8);
+            System.out.println("Tax:" + fitst_tax);
+        } 
     
-       
+            
+            else if (value >= 25000 && value < 55000){
+            double second_tax = (1700 + (value - 25000)*0.10);
+            System.out.println("Tax:" + second_tax);
+         }
+         
+         else if (value >= 55000 && value < 200000){
+            double third_tax = (4700 + (value - 55000)*0.12);
+            System.out.println("Tax:" + third_tax);
+        } 
+            
+            else if (value >= 200000 && value < 1000000){
+            double fourth_tax = (22100 + (value - 200000)*0.15);   
+            System.out.println("Tax:" + fourth_tax);
+        } 
+        
+        else if (value >= 1000000){
+            double fifth_tax = (142100 + (value - 1000000)*0.17);
+            System.out.println("Tax:" + fifth_tax);
+        }
+        
 }
 }
