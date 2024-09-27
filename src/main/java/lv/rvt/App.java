8408edce19055c8416f53b2d5778f84b066739eb
Valērijs Kostjukevičs <166384039+23DP4VKost@users.noreply.github.com>
2024-09-27 10:54:13 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -12,11 +12,45 @@ public class App
         //double floatingPoint = Double.valueOf(scanner.nextLine()); decimal no user
         //boolean trueOrFalse = Boolean.valueOf(scanner.nextLine()); t/f    
 
-    System.out.println("Give speed:");
-    Scanner scanner = new Scanner((System.in));
-    int speed = Integer.valueOf(scanner.nextLine());
-    if (speed > 120) {
-        System.out.println("Speeding ticket!");
-    }
+
+       System.out.println("Give points[0-100]:");
+
+       Scanner scanner = new Scanner(System.in);
+       int grade = Integer.valueOf(scanner.nextLine());
+
+       
+       if (grade < 0 ) {
+        System.out.println("Impossible");
+       } 
+       
+       else if (grade >= 0 && grade <= 49){
+        System.out.println("failed");
+       }
+
+       else if (grade >= 50 && grade <= 59){
+        System.out.println("1");
+       }
+
+       else if (grade >= 60 && grade <= 69){
+        System.out.println("2");
+       }
+
+       else if (grade >= 70 && grade <= 79){
+        System.out.println("3");
+       }
+
+       else if (grade >= 80 && grade <= 89){
+        System.out.println("4");
+       }
+
+       else if (grade <= 100){
+        System.out.println('5');
+       }
+
+       else {
+        System.out.println("incredible!");
+       }
+    
+       
 }
 }
