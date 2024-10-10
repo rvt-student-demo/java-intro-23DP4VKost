ba22b126a35d328c6629947459bb80ce9dca017f
Valērijs Kostjukevičs <166384039+23DP4VKost@users.noreply.github.com>
2024-10-10 11:07:16 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -12,40 +12,27 @@ public class App
         //double floatingPoint = Double.valueOf(scanner.nextLine()); decimal no user
         //boolean trueOrFalse = Boolean.valueOf(scanner.nextLine()); t/f    
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Value of the gift?");
-        double value = Double.valueOf(scanner.nextLine());
-   
-        
-        if (value < 5000) {
-            System.out.println("No tax!");
-        } 
+         Scanner scanner = new Scanner(System.in);
 
-        else if (value >= 5000 && value < 25000){
-            double fitst_tax = (100 + (value - 5000)*0.8);
-            System.out.println("Tax:" + fitst_tax);
-        } 
-    
-            
-            else if (value >= 25000 && value < 55000){
-            double second_tax = (1700 + (value - 25000)*0.10);
-            System.out.println("Tax:" + second_tax);
-         }
-         
-         else if (value >= 55000 && value < 200000){
-            double third_tax = (4700 + (value - 55000)*0.12);
-            System.out.println("Tax:" + third_tax);
-        } 
-            
-            else if (value >= 200000 && value < 1000000){
-            double fourth_tax = (22100 + (value - 200000)*0.15);   
-            System.out.println("Tax:" + fourth_tax);
-        } 
-        
-        else if (value >= 1000000){
-            double fifth_tax = (142100 + (value - 1000000)*0.17);
-            System.out.println("Tax:" + fifth_tax);
+        int numbersRead = 0;
+        int sum = 0;
+        int check = 1;
+
+
+        while(true){
+            if (check == 0) {
+                break;
         }
-        
+
+        System.out.println("Give a number:");
+        int integer = Integer.valueOf(scanner.nextLine());
+        sum = sum + integer;
+        numbersRead = numbersRead + 1;
+        check = integer;
+        }
+
+        numbersRead = numbersRead - 1;
+        System.out.println("Number of numbers:" + numbersRead);
+        System.out.println("The sum of the number is:" + sum);
 }
 }
