354bc6dd49be69ecf78aea453d70ba0a495a222f
Valērijs Kostjukevičs <166384039+23DP4VKost@users.noreply.github.com>
2024-10-10 11:47:28 +0000

    Counting to hundred

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -12,27 +12,10 @@ public class App
         //double floatingPoint = Double.valueOf(scanner.nextLine()); decimal no user
         //boolean trueOrFalse = Boolean.valueOf(scanner.nextLine()); t/f    
 
-         Scanner scanner = new Scanner(System.in);
-
-        int numbersRead = 0;
-        int sum = 0;
-        int check = 1;
-
-
-        while(true){
-            if (check == 0) {
-                break;
-        }
-
-        System.out.println("Give a number:");
+        Scanner scanner = new Scanner(System.in);
         int integer = Integer.valueOf(scanner.nextLine());
-        sum = sum + integer;
-        numbersRead = numbersRead + 1;
-        check = integer;
-        }
-
-        numbersRead = numbersRead - 1;
-        System.out.println("Number of numbers:" + numbersRead);
-        System.out.println("The sum of the number is:" + sum);
+         for(;integer <= 100; integer++){
+            System.out.println(integer);
+         }
 }
 }
