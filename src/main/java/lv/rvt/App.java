e4b5b629bec66c0883ba6cdfd14c9d885e2596a0
Valērijs Kostjukevičs <166384039+23DP4VKost@users.noreply.github.com>
2024-10-18 10:37:21 +0000

    Star sign (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -12,22 +12,81 @@ public class App
         //double floatingPoint = Double.valueOf(scanner.nextLine()); decimal no user
         //boolean trueOrFalse = Boolean.valueOf(scanner.nextLine()); t/f  
 
-//My main method
 
-   divisibleByThreeInRange(2,12);
+
+   printStars(5);
+   printStars(3);
+   printStars(9);
+    space();
+   printSquare(4);
+   space();
+   printRectangle(17,3);
+   space();
+   printTriangle(4);
 }
-public static void divisibleByThreeInRange(int min , int max){
-    while(min <= max){
-        if (min % 3 == 0){
-            System.out.println(min);
-        } else{
-            min++;
-            continue;
+///space
+public static void space(){
+    System.out.println("");
+}
+
+///1.
+public static void printStars(int star)
+{
+
+for(int i = 0;i < star;i++){
+    System.out.print("*");
+}
+    System.out.println("");
+
+
+}
+
+///2.
+public static void printSquare(int size)
+{
+
+for(int table = 0; table < size;table++){
+
+for(int i = 0;i < size;i++){
+    System.out.print("*");
+}
+    System.out.println("");
+
+}
+}
+
+///3.
+
+public static void printRectangle(int width , int heigth)
+{
+
+for(int h = 0;h<heigth;h++){
+
+for(int i = 0;i < width;i++){
+    System.out.print("*");
+}
+    System.out.println("");
+}
+
+}
+
+///4.
+public static void printTriangle(int size)
+ {
+
+for (int x = 0; x <= size; x++)
+    {
+        for (int i = 0; i < x; i++)
+        {
+            System.out.print("*");
         }
-        min += 3;
+        System.out.println("");
+
+
     }
+ }
+
 }
 
 
-}
 
