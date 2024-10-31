5bdafcfa91c60bcca24516eff133b82ffb472448
Valērijs Kostjukevičs <166384039+23DP4VKost@users.noreply.github.com>
2024-10-31 13:16:23 +0000

    Exercise 5 - Reverse Order

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,20 +8,19 @@ public class App
      
     public static void main( String[] args )
     {
-  
+        int sum = 0;
         int[] val = {0, 1, 2, 3}; 
-        int temp;
-     
-        System.out.println( "Original Array: " 
-            + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
         
+        for(int i =0;i<=3;i++){
+            sum = sum + val[i];
+        }
+        
+     
+        System.out.println( "Sum of all numbers = " + sum );
 
-     
-        System.out.println( "Reversed Array: " 
-            + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
-        
-        
-     
+        }
+
+    
     }
  
-}
+
