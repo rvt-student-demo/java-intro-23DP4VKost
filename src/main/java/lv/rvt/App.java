064b169c2e5bfb3003d9cb931b50fb7664ed2140
Valērijs Kostjukevičs <166384039+23DP4VKost@users.noreply.github.com>
2024-10-31 12:44:42 +0000

    Exercise 1 - Array Sum

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,24 +9,16 @@ public class App
     public static void main( String[] args )
     {
   
-        int[] valA   = { 13, -22,  82,  17}; 
-        int[] valB   = {  0,   0,   0,   0};
-        
-        valB[0] = 12;
-        valB[1] = 47;
-        valB[2] = -57;
-        valB[3] = 8;
-        
-        System.out.println( "valA: " 
-            + valA[0] + " " + valA[1] + " " + valA[2] + " " + valA[3] );
+        int[] val = {0, 1, 2, 3}; 
+        int temp;
      
-        System.out.println( "valB: " 
-            + valB[0] + " " + valB[1] + " " + valB[2] + " " + valB[3] );
-    
-        System.out.println( "sum:  " 
-            + (valA[0]+valB[0]) + " " + (valA[1]+valB[1]) + " " 
-            + (valA[2]+valB[2]) + " " + (valA[3]+valB[3]) );      
+        System.out.println( "Original Array: " 
+            + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
         
+
+     
+        System.out.println( "Reversed Array: " 
+            + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
         
         
      
