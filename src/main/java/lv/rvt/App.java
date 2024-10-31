7936077cbe6d569a09cce70f55fec12ec7c72f10
Valērijs Kostjukevičs <166384039+23DP4VKost@users.noreply.github.com>
2024-10-31 12:26:39 +0000

    Exercise 2 - Two Arrays

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,23 +8,28 @@ public class App
      
     public static void main( String[] args )
     {
-        int sum = 0;
-        int[] val = {0, 1, 2, 3}; 
+  
         
-        for(int i =0;i<=3;i++){
-            sum = sum + val[i];
-        }
+        
+        
+            int[] val = {13, -4, 82, 17}; 
+            int[] twice = new int [4];
+            
+            System.out.println( "Original Array: " 
+                + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+          
+            for(int i=0;i<=3;i++){
+                twice[i] = val[i] * 2;
+            }
+         
+            System.out.println( "New Array: " 
+                + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+           
+        
+        
+        
         
      
-        System.out.println( "Sum of all numbers = " + sum );
-
-        }
-
-    
-
-    
-
-
-
-
+    }
+ 
 }
