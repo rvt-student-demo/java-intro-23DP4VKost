c76dd569db55cc4ebe79cc0983cc94c86e36eafc
Valērijs Kostjukevičs <166384039+23DP4VKost@users.noreply.github.com>
2024-11-09 16:42:26 +0000

    Decreasing Counter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,18 +1,14 @@
 package lv.rvt;
 
-public class App 
-{
-    public class Main 
-    {
-        public static void main(String[] args) 
-        {
-            Product Product1 = new Product("Banana",1.1,13);
-            Product Product2 = new Product("Gruwa",1.5,7);
+public class App {
 
-            Product1.printProduct();
-            System.out.println();
-            Product2.printProduct();
-            
-        }
+    public static void main(String[] args) {
+        DecreasingCounter counter = new DecreasingCounter(100);
+        counter.printValue();
+        counter.reset();
+        counter.printValue();
+        counter.decrement();
+        counter.printValue();
+        
     }
 }
